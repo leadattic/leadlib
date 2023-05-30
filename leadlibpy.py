0f3186b79d0092d05b2d20c13ea783b04b030995
leadattic <90087384+leadattic@users.noreply.github.com>
2023-05-30 16:39:56 +0200

    Update leadlibpy.py

--- leadlibpy.py
+++ leadlibpy.py
@@ -1,1 +1,13 @@
+import os
 
+def dir(path, type):
+  if type == "c" or type == "C":
+    os.mkdir(path)
+  elif type == "e" or type == "E":
+    if os.path.exists(path):
+      return True
+    else:
+      return False
+  else:
+    return "Type of function does not exist"
+   
